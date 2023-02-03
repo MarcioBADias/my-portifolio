34be0d3c62c0db997808b6276e90fb42b400e5aa
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-02 21:22:11 -0300

    Atualizando o index.js para consertar deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-root.render(<App />)
+reportWebVitals();
